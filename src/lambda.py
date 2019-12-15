d9b5d695f194c4f55ac65eaebed08dc7faf6a0db
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2019-12-15 18:55:47 +0000

    updated contents of the lambda function

--- src/lambda.py
+++ src/lambda.py
@@ -7,22 +7,14 @@ ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime = boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
     body = json.loads(event['body'])
-    print(ENDPOINT_NAME)
-    message = 'Hello, my name is {} and {}'.format(body["firstname"], body["surname"])
-    #response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
-    #                                   ContentType='text/csv',
-    #                                   Body=payload)
-    #print(response)
-    #result = json.loads(response['Body'].read().decode())
-    #print(result)
-    #pred = int(result['predictions'][0]['predicted_label'])
-    
-    #return pred
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='text/csv',
+                                       Body=body['data'])
+    result = json.loads(response['Body'].read().decode())
     return {
         'statusCode' : 200,
         'headers' : { 'Content-Type' : 'application/json', 'Access-Control-Allow-Origin' : '*' },
-        'body' : json.dumps({'code': 200, 'message': message})
+        'body' : json.dumps({'code': 200, 'message': result})
     }
 
